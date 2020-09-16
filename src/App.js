5e11ade90153fffd239ae9e61e84e14f127e24e0
Andrea <aganesh0988@gmail.com>
2020-09-15 23:57:19 -0500

    added event handlers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+// function App() {
+//   return (
+//     <h1></h1>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,22 +1,46 @@
 import React from 'react';
 
 class ContactUs extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+    };
+  }
+
+  nameOnChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
+  emailOnChange = (e) => {
+    this.setState({ email: e.target.value });
+  }
+
+  phoneOnChange = (e) => {
+    this.setState({ phone: e.target.value });
+  }
+
   render() {
+    const { name, email, phone } = this.state;
+
     return (
       <div>
         <h2>Contact Us</h2>
         <form>
           <div>
             <label htmlFor='name'>Name:</label>
-            <input id='name' type='text' />
+            <input id='name' type='text' onChange={this.nameOnChange} value={name} />
           </div>
           <div>
             <label htmlFor='email'>Email:</label>
-            <input id='email' type='text' />
+            <input id='email' type='text' onChange={this.emailOnChange} value={email} />
           </div>
           <div>
             <label htmlFor='phone'>Phone:</label>
-            <input id='phone' type='text' />
+            <input id='phone' type='text' onChange={this.phoneOnChange} value={phone} />
           </div>
           <div>
             <button>Submit</button>
