bcf56ae14e05b1a65a5bc3a4185e6148d8c4606a
Andrea <aganesh0988@gmail.com>
2020-09-16 00:02:48 -0500

    prevented default behavior

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -23,13 +23,42 @@ class ContactUs extends React.Component {
     this.setState({ phone: e.target.value });
   }
 
+  onSubmit = (e) => {
+    // Prevent the default form behavior
+    // so the page doesn't reload.
+    e.preventDefault();
+
+    // Retrieve the contact us information from state.
+    const { name, email, phone } = this.state;
+
+    // Create a new object for the contact us information.
+    const contactUsInformation = {
+      name,
+      email,
+      phone,
+      submittedOn: new Date(),
+    };
+
+    // For now, just log the contact us information to the console
+    // though ideally, we'd persist this information to a database
+    // using a REST API.
+    console.log(contactUsInformation);
+
+    // Reset the form state.
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+    });
+  }
+
   render() {
     const { name, email, phone } = this.state;
 
     return (
       <div>
         <h2>Contact Us</h2>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label htmlFor='name'>Name:</label>
             <input id='name' type='text' onChange={this.nameOnChange} value={name} />
