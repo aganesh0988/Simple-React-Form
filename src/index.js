aa8e82a139da0bb506fc8a4cb20faf11666902c0
Andrea <aganesh0988@gmail.com>
2020-09-15 23:51:18 -0500

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ContactUs from './ContactUs';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContactUs />
   </React.StrictMode>,
   document.getElementById('root')
 );
